15e2924612bda3c77fc06420698a41418453b600
cleber <cb83silva@gmail.com>
2023-11-10 10:16:20 -0300

    najuste routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/contato', function () {
+    return view('contato');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
